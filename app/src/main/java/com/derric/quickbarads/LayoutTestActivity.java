9cae2426f846008f9d5d63591e4af258104ef400
Derric Dani George <derricdani@gmail.com>
2022-05-02 19:26:15 +0530

    Package rename and Banner ads homescreen

---
+++ app/src/main/java/com/derric/quickbarads/LayoutTestActivity.java
@@ -0,0 +1,14 @@
+package com.derric.quickbarads;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LayoutTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.testlayout);
+    }
+}
